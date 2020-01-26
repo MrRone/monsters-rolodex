3cd0fdc121246bbeaef360942ad7f76600710c06
Ty <tyrone_458@hotmail.com>
2020-01-26 20:04:22 +0000

    Updates

---
+++ precache-manifest.67e9b634f50af282212ada9c3a3504b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb64120e670a09e6c2a932af6d937e1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fdd89be1508ecb7bb715",
+    "url": "/monsters-rolodex/static/css/main.073814dd.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fdd89be1508ecb7bb715",
+    "url": "/monsters-rolodex/static/js/main.fb7fb486.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
