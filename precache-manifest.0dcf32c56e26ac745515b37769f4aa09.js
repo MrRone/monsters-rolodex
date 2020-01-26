83f2115c4cc1939064edbd4bf3d3082e3a54b63c
Ty <tyrone_458@hotmail.com>
2020-01-26 20:04:01 +0000

    Updates

---
+++ precache-manifest.0dcf32c56e26ac745515b37769f4aa09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8284f7f80415ae55086d7b714594de2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a09e10a6eb05cec1a1af",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b49ede963a0f2ff5b4ef",
+    "url": "/static/js/2.62dce5ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.62dce5ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a09e10a6eb05cec1a1af",
+    "url": "/static/js/main.a69cae52.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
